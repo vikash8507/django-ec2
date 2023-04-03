e186c50657ff7322a07425e31097f7774609d6af
Vikash Kumar <vikas.kumar@mitratech.com>
2023-04-03 15:19:35 +0530

    Added env for django secret key

--- django_ec2/settings.py
+++ django_ec2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1b3wamd^cst$e&r=uluty=b&2vazrvqf&k5l4te&(#i=lk#f$6'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+if not SECRET_KEY:
+    raise Exception("Django Secret Key is not provided in env")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
